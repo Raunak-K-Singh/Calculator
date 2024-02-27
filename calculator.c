1078a0aa229af126cafe1c9a20676772bc32ab7c
Raunak-K-Singh <106871251+Raunak-K-Singh@users.noreply.github.com>
2024-02-27 17:53:31 +0530

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
+
+
+int main(int argc,string argv[]) {
+    if (argc > 1000000000 || argc % 2 != 0) {
+        printf("Usage: %s <num1> <op1> <num2> [<op2> <num3> ...]\n", argv[0]);
+        return 1; // Refer Readme.
+    }
+
+    double result = atof(argv[1]); // Initialize result with the first number
+
+    for (int i = 2; i < argc; i += 2) {
+        char operator = argv[i][0];
+        double operand = atof(argv[i + 1]);
+
+        switch (operator) {
+            case '+':
+                result += operand;
+                break;
+            case '-':
+                result -= operand;
+                break;
+            case '*':
+                result *= operand;
+                break;
+            case '/':
+                if (operand != 0) {
+                    result /= operand;
+                } else {
+                    printf("Error: Division by zero\n");
+                    return 2; // Refer Readme.
+                }
+                break;
+            default:
+                printf("Error: Invalid operator '%c'\n", operator);
+                return 3; // Refer Readme.
+        }
+    }
+
+    printf("Result: %.2lf\n", result);
+
+    return 0;
+}
